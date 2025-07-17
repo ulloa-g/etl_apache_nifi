e62402245e710fbd9b3058a758de43d8843d65b9
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-07-17 20:10:20 -0300

    pep8 compliance

--- script.py
+++ script.py
@@ -1,14 +1,17 @@
 import pandas as pd
 
+
 def extract(path):
     df = pd.read_csv(path)
     return df
+
 
 def transform(df):
     df['married'] = df['married'].map({'TRUE': 1, 'FALSE': 0, 'yes': 1, 'no': 0, '0': 0, '1': 1})
     df['married'] = df['married'].fillna(0)
     df['married'] = df['married'].astype(int)
     return df
+
 
 if __name__ == "__main__":
     path_to_data = "/home/gabriel/repos/etl_apache_nifi/input/data.csv"
