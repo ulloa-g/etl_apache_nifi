efc87790a72a91d59adbab48177accd5486918ca
ulloagabriel <ulloasaavedragabriel@gmail.com>
2025-07-17 20:08:19 -0300

    fix married col

--- script.py
+++ script.py
@@ -4,7 +4,13 @@ def extract(path):
     df = pd.read_csv(path)
     return df
 
+def transform(df):
+    df['married'] = df['married'].map({'TRUE': 1, 'FALSE': 0, 'yes': 1, 'no': 0, '0': 0, '1': 1})
+    df['married'] = df['married'].fillna(0)
+    df['married'] = df['married'].astype(int)
+    return df
+
 if __name__ == "__main__":
     path_to_data = "/home/gabriel/repos/etl_apache_nifi/input/data.csv"
     raw_df = extract(path_to_data)
-    print(raw_df.head(10))
+    cleaned_df = transform(raw_df)
